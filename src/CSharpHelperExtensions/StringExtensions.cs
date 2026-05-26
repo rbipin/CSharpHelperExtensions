2ed146101ee4a862a43d2a42c8f34aa64fcef2bd
Bipin Radhakrishnan <bipinrknr@gmail.com>
2026-05-26 18:08:29 -0400

    reorganize files code to /src

---
+++ src/CSharpHelperExtensions/StringExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace CSharpHelperExtensions.Strings;
+
+public static class StringExtensions
+{
+    /// <summary>
+    /// Convert Nullable type
+    /// </summary>
+    /// <param name="input"></param>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    public static T? ToNullable<T>(this string input) where T : struct
+    {
+        try
+        {
+            if (input.IsNullOrEmpty())
+            {
+                return null;
+            }
+
+            var conv = TypeDescriptor.GetConverter(typeof(T));
+            return (T?)conv.ConvertFrom(input);
+        }
+        catch (Exception e)
+        {
+            throw;
+        }
+    }
+}
+
