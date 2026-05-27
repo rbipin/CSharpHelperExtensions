35ec1825ff3c48ac473fe72e794cd47bad856a55
Bipin Radhakrishnan <bipinrknr@gmail.com>
2026-05-26 21:35:33 -0400

    docs: improve XML documentation on StringExtensions.ToNullable

--- src/CSharpHelperExtensions/StringExtensions.cs
+++ src/CSharpHelperExtensions/StringExtensions.cs
@@ -6,11 +6,32 @@ namespace CSharpHelperExtensions.Strings;
 public static class StringExtensions
 {
     /// <summary>
-    /// Convert Nullable type
+    /// Converts a string to the specified nullable value type using its registered <see cref="TypeConverter"/>.
+    /// Returns <see langword="null"/> when the input is <see langword="null"/>, empty, or whitespace.
     /// </summary>
-    /// <param name="input"></param>
-    /// <typeparam name="T"></typeparam>
-    /// <returns></returns>
+    /// <param name="input">The string to convert.</param>
+    /// <typeparam name="T">
+    /// The target value type (e.g. <see cref="int"/>, <see cref="DateTime"/>, <see cref="Guid"/>).
+    /// Must be a struct; the return type is <typeparamref name="T"/>?.
+    /// </typeparam>
+    /// <returns>
+    /// The converted <typeparamref name="T"/> value wrapped in a nullable,
+    /// or <see langword="null"/> if <paramref name="input"/> is <see langword="null"/>, empty, or whitespace.
+    /// </returns>
+    /// <exception cref="Exception">
+    /// Rethrows any exception thrown by the underlying <see cref="TypeConverter"/> when the string
+    /// cannot be parsed as <typeparamref name="T"/> (e.g. <see cref="FormatException"/>
+    /// or <see cref="NotSupportedException"/>).
+    /// </exception>
+    /// <example>
+    /// <code>
+    /// "42".ToNullable&lt;int&gt;()                  // (int?)42
+    /// "".ToNullable&lt;int&gt;()                    // null
+    /// "   ".ToNullable&lt;int&gt;()                 // null (whitespace)
+    /// "2024-01-15".ToNullable&lt;DateTime&gt;()    // (DateTime?)new DateTime(2024, 1, 15)
+    /// "not-a-number".ToNullable&lt;int&gt;()       // throws FormatException
+    /// </code>
+    /// </example>
     public static T? ToNullable<T>(this string input) where T : struct
     {
         try
