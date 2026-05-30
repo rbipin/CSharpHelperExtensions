f3dbbeb94800e8370dc76ca41f005415e87a2298
Bipin Radhakrishnan <bipinrknr@gmail.com>
2026-05-30 01:59:51 -0400

    add GetOrAdd to DictionaryExtensions

--- src/CSharpHelperExtensions.Test/DictionaryExtensionTest.cs
+++ src/CSharpHelperExtensions.Test/DictionaryExtensionTest.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using CSharpHelperExtensions.Dictionaries;
 using Shouldly;
@@ -7,4 +8,29 @@ namespace CSharpHelperExtensions.Test;
 
 public class DictionaryExtensionTest
 {
+    [Fact]
+    public void GetOrAdd_KeyExists_ReturnsExistingValue()
+    {
+        var dict = new Dictionary<string, int> { ["a"] = 1 };
+        var factoryCalled = false;
+        var result = dict.GetOrAdd("a", _ => { factoryCalled = true; return 99; });
+        result.ShouldBe(1);
+        factoryCalled.ShouldBeFalse();
+    }
+
+    [Fact]
+    public void GetOrAdd_KeyMissing_InvokesFactoryAddsAndReturnsValue()
+    {
+        var dict = new Dictionary<string, int>();
+        var result = dict.GetOrAdd("b", key => 42);
+        result.ShouldBe(42);
+        dict["b"].ShouldBe(42);
+    }
+
+    [Fact]
+    public void GetOrAdd_NullFactory_Throws()
+    {
+        var dict = new Dictionary<string, int>();
+        Should.Throw<ArgumentNullException>(() => dict.GetOrAdd("a", null!));
+    }
 }
--- src/CSharpHelperExtensions/DictionaryExtensions.cs
+++ src/CSharpHelperExtensions/DictionaryExtensions.cs
@@ -7,4 +7,19 @@ namespace CSharpHelperExtensions.Dictionaries;
 
 public static class DictionaryExtensions
 {
+    public static TValue GetOrAdd<TKey, TValue>(
+        this IDictionary<TKey, TValue> dict,
+        TKey key,
+        Func<TKey, TValue> factory)
+    {
+        ArgumentNullException.ThrowIfNull(key);
+        ArgumentNullException.ThrowIfNull(factory);
+
+        if (dict.TryGetValue(key, out var existing))
+            return existing;
+
+        var value = factory(key);
+        dict[key] = value;
+        return value;
+    }
 }
